3a047f79ddbf1a46c7a34346d68abf46362a19c0
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2023-12-10 23:33:50 +0530

    Time: 76 ms (42.59%), Space: 17.3 MB (22.76%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -3,9 +3,27 @@ class Solution:
         l=[]
         m=len(matrix)
         n=len(matrix[0])
-        for i in range(n):
-            x=[]
-            for j in range(m):
-                x.append(matrix[j][i])
-            l.append(x)
-        return l
+        if m==n:
+            for i in range(n):
+                for j in range(i+1,n):
+                    matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        elif n>m:
+            for i in range(m):
+                for j in range(i+1,m):
+                    matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+            while n>m:
+                x=[]
+                for i in range(m):
+                    x.append(matrix[i].pop())
+                matrix.insert(m,x)
+                n-=1
+        else:
+            for i in range(n):
+                for j in range(i+1,n):
+                    matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+            while m>n:
+                x=matrix.pop(n)
+                for i in range(n):
+                    matrix[i].append(x[i])
+                m-=1
+        return matrix
