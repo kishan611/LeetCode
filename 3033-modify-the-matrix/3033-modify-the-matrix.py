670f01f7782431d1a9c0cdc85013076be172fbf3
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-02-14 23:07:54 +0530

    Time: 68 ms (90.75%), Space: 16.6 MB (50.58%) - LeetHub

--- 3033-modify-the-matrix/3033-modify-the-matrix.py
+++ 3033-modify-the-matrix/3033-modify-the-matrix.py
@@ -1,6 +1,5 @@
 class Solution:
     def modifiedMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
-        maxc=[]
         m=len(matrix)
         n=len(matrix[0])
         for i in range(n):
@@ -8,10 +7,8 @@ class Solution:
             for j in range(1,m):
                 if cm<matrix[j][i]:
                     cm=matrix[j][i]
-            maxc.append(cm)
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j]==-1:
-                    matrix[i][j]=maxc[j]
+            for j in range(m):
+                if matrix[j][i]==-1:
+                    matrix[j][i]=cm
         return matrix
         
