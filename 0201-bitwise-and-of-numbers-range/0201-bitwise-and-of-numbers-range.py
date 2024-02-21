4b68180a933c3f2fce8d2bb8828c2f2447f5989f
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-02-21 13:25:52 +0530

    Time: 76 ms (10.27%), Space: 16.8 MB (49.69%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -1,21 +1,11 @@
 from math import log
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        if left==0:
-            return 0
-        if left==right:
-            return left
-        x=log(left,2)
-        y=int(log(right,2))
-        if int(x)!=y:
-            return 0
-        if x-int(x)==0:
-            return 2**int(x)
-        y=2**int(x)
-        x=left
-        i=left+1
-        while i<right+1 and x!=y:
-            x=x&i
-            i+=1
-        return x
+        count=0
+        while left!=right:
+            left>>=1
+            right>>=1
+            count+=1
+            print(left,right)
+        return left<<count
         
