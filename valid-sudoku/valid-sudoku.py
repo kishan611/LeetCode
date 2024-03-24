b35a277914b360a217e8991f4d6c1f65ecdace3b
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-03-24 19:10:28 +0530

    Time: 94 ms, Memory: 16.5 MB - LeetHub

--- valid-sudoku/valid-sudoku.py
+++ valid-sudoku/valid-sudoku.py
@@ -1,8 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        res=[]
+        dr={i:[] for i in range(9)}
+        dc={j:[] for j in range(9)}
+        d9={(i,j):[] for i in range(3) for j in range(3)}
         for i,row in enumerate(board):
             for j,val in enumerate(row):
                 if val!=".":
-                    res+=[(i,val),(val,j),(i//3,j//3,val)]
-        return len(res)==len(set(res))
+                    if val in dr[i]:
+                        return False
+                    dr[i].append(val)
+                    if val in dc[j]:
+                        return False
+                    dc[j].append(val)
+                    if val in d9[(i//3,j//3)]:
+                        return False
+                    d9[(i//3,j//3)].append(val)
+        return True
