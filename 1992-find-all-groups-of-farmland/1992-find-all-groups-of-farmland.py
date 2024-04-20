a0468ac20d411e3b291d9627a768b3321ec30797
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-04-20 12:11:38 +0530

    Time: 757 ms (91.00%), Space: 30.6 MB (60.50%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -3,20 +3,17 @@ class Solution:
         m=len(land)
         n=len(land[0])
         res=[]
-        def dfs(i,j,rmax,cmax):
-            if i<0 or i>=m or j<0 or j>=n or land[i][j]==0:
-                return rmax,cmax
-            land[i][j]=0
-            rmax=max(rmax,i)
-            cmax=max(cmax,j)
-            rmax,cmax=dfs(i,j+1,rmax,cmax)
-            rmax,cmax=dfs(i+1,j,rmax,cmax)
-            return rmax,cmax
         for i in range(m):
             for j in range(n):
                 if land[i][j]==1:
-                    rmax,cmax=dfs(i,j,-1,-1)
-                    res.append([i,j,rmax,cmax])
+                    x=i
+                    while x<m and land[x][j]==1:
+                        y=j
+                        while y<n and land[x][y]==1:
+                            land[x][y]=0
+                            y+=1
+                        x+=1   
+                    res.append([i,j,x-1,y-1])
         return res
                         
                 
