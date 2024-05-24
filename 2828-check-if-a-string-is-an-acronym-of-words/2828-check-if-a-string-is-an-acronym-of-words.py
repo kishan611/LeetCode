8223e34e29effd168864d8099e1011c8e31989b7
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-05-24 12:24:37 +0530

    Time: 49 ms (82.57%), Space: 16.5 MB (94.89%) - LeetHub

--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.py
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.py
@@ -1,10 +1,4 @@
 class Solution:
     def isAcronym(self, words: List[str], s: str) -> bool:
-        n=len(words)
-        m=len(s)
-        if n!=m:
-            return False
-        for i in range(n):
-            if s[i]!=words[i][0]:
-                return False
-        return True
+        x="".join([x[0] for x in words])
+        return x==s
