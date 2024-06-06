a07bca6bd1e3ceb3b1c9fa67b6b4b5eb2a767d02
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-06-06 23:56:09 +0530

    Time: 514 ms (10.43%), Space: 18.5 MB (50.76%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -5,17 +5,14 @@ class Solution:
         if n%groupSize!=0:
             return False
         hand=Counter(hand)
-        while n>0:
-            for i,j in sorted(hand.items()):
-                for k in range(i,i+groupSize):
-                    if hand.get(k,0)==0:
-                        return False
-                    hand[k]-=1
-                    if hand[k]==0:
-                        del hand[k]
-                if 1:
-                    break
-            n-=1
+        while hand:
+            x=min(hand)
+            for i in range(x,x+groupSize):
+                if not hand.get(i,0):
+                    return False
+                hand[i]-=1
+                if hand[i]==0:
+                    del hand[i]
         return True
         
         
