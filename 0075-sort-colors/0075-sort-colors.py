33d1b53c94e596a253860893cc3a8e63e0b72265
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-06-12 11:18:19 +0530

    Time: 37 ms (57.63%), Space: 16.6 MB (7.83%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,5 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        a=Counter(nums)
+        z=a.get(0,0)
+        o=a.get(1,0)
+        t=a.get(2,0)
+        for i in range(z):
+            nums[i]=0
+        for i in range(z,z+o):
+            nums[i]=1
+        for i in range(z+o,z+o+t):
+            nums[i]=2
         
