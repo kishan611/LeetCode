38c1ac9ca52bf112ee8a0b58035442fcb48f5c9f
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-08-06 11:46:18 +0530

    Time: 200 ms (42.00%), Space: 17.6 MB (36.75%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,9 +1,8 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        d={}
+        res=[0]*26
         for i in word:
-            d[i]=d.get(i,0)+1
-        res=list(d.values())
+            res[ord(i)-97]+=1
         res.sort(reverse=True)
         ans=sum(res[:8])+(sum(res[8:16])*2)+(sum(res[16:24])*3)+(sum(res[24:])*4)
         return ans
