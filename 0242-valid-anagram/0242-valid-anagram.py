db630d53fe70c59dbc527dca118627c578c71dc0
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-08-15 13:16:14 +0530

    Time: 51 ms (47.17%), Space: 17.4 MB (10.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        t=list(t)
-        for i in s:
-            if i in t:
-                t.remove(i)
-            else:
-                return False
-        if t:
-            return False
-        return True
+        if sorted(s)==sorted(t):
+            return True
+        return False
