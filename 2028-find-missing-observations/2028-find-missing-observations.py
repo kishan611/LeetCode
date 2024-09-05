ba105efc06462bf7a554ed5e121fdadb162d53f7
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-09-05 09:33:19 +0530

    Time: 991 ms (56.68%), Space: 27.4 MB (27.01%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.py
+++ 2028-find-missing-observations/2028-find-missing-observations.py
@@ -4,16 +4,8 @@ class Solution:
         nsum=(m+n)*mean-sum(rolls)
         if nsum<n or nsum>n*6:
             return []
-        res=[1]*n
-        nsum-=n
-        i=0
-        while nsum>=5:
-            res[i]+=5
-            nsum-=5
-            i+=1
-        if nsum:
-            res[i]+=nsum
-        return res
+        q,r=nsum//n,nsum%n
+        return [q+(1 if i<r else 0) for i in range(n)]
 
 
         
