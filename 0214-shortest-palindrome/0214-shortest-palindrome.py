5931d38037fb83fa23964dbe856d21fd14f12f05
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-09-21 00:01:24 +0530

    Time: 56 ms (82.70%), Space: 17 MB (64.78%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,8 +1,6 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        x=s[::-1]
-        for i in range(len(x)+1):
-            res=x[:i]+s
-            if res==res[::-1]:
-                return res
-        
+        res = s[::-1]
+        for i in range(len(s) + 1):
+            if s.startswith(res[i:]):
+                return res[:i] + s
