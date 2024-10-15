2f169415745148167b3c6ae6fda0eeb271613ee9
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-10-15 10:28:40 +0530

    Time: 85 ms (75.22%), Space: 17.7 MB (20.90%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        c0=c1=0
+        res=0
+        for i in s:
+            if i=='0':
+                res+=c1
+            else:
+                c1+=1
+        return res
+        
