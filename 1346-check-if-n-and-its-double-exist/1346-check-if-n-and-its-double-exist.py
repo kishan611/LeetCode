adc87231fd04f8c9c97da759e89ad9bb734c9c55
Kishan Agarwal <109837928+kishan611@users.noreply.github.com>
2024-12-01 17:28:19 +0530

    Time: 0 ms (100.00%), Space: 17.3 MB (5.25%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        s=set()
+        for i in arr:
+            if i*2 in s or i/2 in s:
+                return True
+            s.add(i)
+        return False
+        
